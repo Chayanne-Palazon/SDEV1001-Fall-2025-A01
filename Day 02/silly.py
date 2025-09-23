0afdadfec8339c30943a54edb4492e6bac7f41dd
Chayanne-Palazon <cpalazon1@nait.ca>
2025-09-23 10:07:56 -0600

    changes

---
+++ Day 02/silly.py
@@ -0,0 +1,6 @@
+name = input("Name: ")
+adj = input("Adjective: ")
+verb = input("Verb (Past Tense): ")
+place = input("Place: ")
+
+print(name +" "+ verb +" to the " + place + " with a " + adj + " grin")
---
+++ Day 02/temp.py
@@ -0,0 +1,5 @@
+ask_celsius_str = input("Temperature: ")
+ask_celsius = int(ask_celsius_str)
+convert = (ask_celsius * 9/5) + 32
+
+print("Celsius:",ask_celsius,"C => Fahrenheit:",convert,"F")
